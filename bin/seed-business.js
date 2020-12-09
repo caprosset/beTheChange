8247de5464217a4db46cc064be040d6f671db68e
Alberto <alpagor@gmail.com>
2020-12-09 15:56:54 +0100

    models testing first commit

---
+++ bin/seed-business.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Business = require("../models/business");
+
+const business = [
+  {
+    name: '',
+    url: '',
+    img: '',
+    description: '',
+    categories: [],
+  },
+];
